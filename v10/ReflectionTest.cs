437ae90194d340d935256def65a9f0c7bb7eb703
Marcus Newman <marcusgeorgenewman@gmail.com>
2015-04-30 16:08:40 -0700

    Rechecking in VS2008 version

---
+++ v10/ReflectionTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MGN.ReflectionTests
+{
+    /// <summary>
+    /// Unit test methods that use reflection.
+    /// </summary>
+    public static class ReflectionTest
+    {
+        // TODO: test this for internal vs external testing relative paths
+        //TODO: just check both paths
+        /// <summary>
+        /// Tests for the existance of an assembly using reflection.
+        /// </summary>
+        /// <param name="assemblyName">Name of the assembly.</param>
+        /// <returns>The assembly.</returns>
+        /// <exception cref="Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException">Thrown when the assembly is not found.</exception>
+        public static Assembly Assembly(string assemblyName)
+        {
+            var currentDirectory = System.Environment.CurrentDirectory;
+            var relativeAssemblyPath = String.Format("..\\..\\..\\..\\{0}\\bin\\Debug\\{0}.dll", assemblyName);
+            //..\\..\\..\\{0}\\bin\\Debug\\{0}.dll", assemblyName);
+            //"..\\..\\..\\bin\\Debug\\{0}.dll", assemblyName);
+
+            try
+            {
+                return System.Reflection.Assembly.LoadFrom(relativeAssemblyPath);
+            }
+            catch (FileNotFoundException fileNotFoundException)
+            {
+                throw new AssertFailedException(assemblyName + " assembly was not found.", fileNotFoundException);
+            }
+        }
+        /*
+       /// <summary>
+       /// Tests for the existance of a class type using reflection.
+       /// </summary>
+       /// <param name="assemblyName">Name of the assembly.</param>
+       /// <param name="className">Name of the class.</param>
+       /// <returns>The Type of the class.</returns>
+       /// <exception cref="Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException">Thrown when the assembly or class is not found.</exception>
+       public static Type Class(String assemblyName, String className)
+       {
+           var assembly = Assembly.Test(assemblyName: assemblyName);
+           try { return assembly.GetType(assembly.GetName().Name + "." + className, true); }
+           catch (TypeLoadException typeLoadException)
+           { throw new Exception(className + " class was not found.", typeLoadException); }
+       }
+
+       /// <summary>
+       /// Tests for the existance of a method using reflection.
+       /// </summary>
+       /// <param name="assemblyName">Name of the assembly.</param>
+       /// <param name="className">Name of the class.</param>
+       /// <param name="methodName">Name of the method.</param> 
+       /// <returns>The MethodInfo of the method.</returns>
+       /// <exception cref="Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException">Thrown when the assembly, class or method is not found.</exception>
+       public static r.MethodInfo Method(String assemblyName, String className, String methodName)
+       {
+           var classType = Class(assemblyName: assemblyName, className: className);
+           var methodInfo = classType.GetMethod(name: methodName);
+           if (methodInfo == null) throw new Exception(methodName + " method was not found.");
+           return methodInfo;
+       }
+    */
+    }
+}
+
---
+++ v10/Tests/ReflectionTestTests.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MGN.ReflectionTests.Tests
+{
+    [TestClass]
+    public class ReflectionTestTests
+    {
+        const string AssemblyName = "MGN.ReflectionTests";
+
+        /// <summary>
+        /// Tests for the existance of the MGN.ReflectionTests assembly.
+        /// </summary>
+        [TestMethod]
+        public void MGN_ReflectionTests_assembly_should_exist()
+        {
+            GetAssembly();
+        }
+
+        private static Assembly GetAssembly()
+        {
+            Assembly assembly = null;
+            try
+            {
+                assembly = Assembly.LoadFrom(assemblyFile: "..\\..\\..\\bin\\Debug\\" + AssemblyName + ".dll");
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Assert.Fail(message: AssemblyName + " assembly was not found.");
+            }
+            return assembly;
+        }
+
+        /// <summary>
+        /// Tests for the existance of the ReflectionTest class.       
+        /// </summary>
+        [TestMethod]
+        public void ReflectionTest_class_should_exist()
+        {
+            GetClasstype();
+        }
+
+        private static Type GetClasstype()
+        {
+            var assembly = GetAssembly();
+            var classtype = assembly.GetType(AssemblyName + ".ReflectionTest");
+            Assert.IsNotNull(value: classtype, message: "The ReflectionTest class should exist.");
+            return classtype;
+        }
+
+        /// <summary>
+        /// Tests for the existance of the Assembly method.
+        /// </summary>
+        [TestMethod]
+        public void Assembly_method_should_exist()
+        {
+            GetAssemblyMethod();
+        }
+
+        private static MethodInfo GetAssemblyMethod()
+        {
+            var methodName = "Assembly";
+            var parameterTypes = new Type[] { typeof(String) };
+            var returnType = typeof(Assembly);
+            return GetMethod(methodName: methodName, parameterTypes: parameterTypes, returnType: returnType);
+        }
+
+        private static MethodInfo GetMethod(string methodName, Type[] parameterTypes, Type returnType)
+        {
+            var classtype = GetClasstype();
+            var methodInfo = classtype.GetMethod(name: methodName);
+            var methodParameters = methodInfo.GetParameters();
+            Assert.IsTrue(condition: methodParameters.Length == parameterTypes.Length, message: methodName + " method should accept " + parameterTypes.Length + " parameter(s).");
+            for (var i = 0; i < methodParameters.Length; i++)
+            {
+                Assert.IsTrue(condition: methodParameters[i].ParameterType == parameterTypes[i], message: methodName + " parameter " + i.ToString() + " should be of type " + parameterTypes[i].ToString() + ".");
+            }
+            Assert.IsTrue(condition: methodInfo.IsStatic, message: methodName + " method should be static.");
+            Assert.IsTrue(condition: methodInfo.ReturnType == returnType, message: methodName + " method return type should be " + returnType.ToString() + ".");
+            return methodInfo;
+        }
+
+        /// <summary>
+        /// Assembly method should fail with an invalid assembly name.
+        /// </summary>
+        [TestMethod]
+        public void Assembly_method_should_fail_with_an_invalid_assembly_name()
+        {
+            var methodInfo = GetAssemblyMethod();
+            var parameters = new String[] { "InvalidAssemblyName" };
+            TestAssemblyMethod(methodInfo: methodInfo, parameters: parameters, shouldFail: true);
+        }
+
+
+
+
+        private Assembly TestAssemblyMethod(MethodInfo methodInfo, Object[] parameters = null, Boolean shouldFail = false)
+        {
+            return (Assembly)InvokeMethod(methodInfo: methodInfo, parameters: parameters, shouldFail: shouldFail);
+        }
+
+
+
+
+
+
+
+        //TODO: Fix relative path issue
+        /// <summary>
+        /// Assembly method should return an instance of an Assembly with a valid assembly name.
+        /// </summary>
+        [TestMethod]
+        public void Assembly_method_should_return_an_Assembly()
+        {
+            var methodInfo = GetAssemblyMethod();
+            var parameters = new String[] { "MGN.ReflectionTests" };
+            TestAssemblyMethod(methodInfo: methodInfo, parameters: parameters, shouldFail: false);
+        }
+
+
+
+        private static Object InvokeMethod(MethodInfo methodInfo, Object[] parameters, Boolean shouldFail = false)
+        {
+            object resultObject = null;
+            Exception caughtException = null;
+            try
+            {
+                resultObject = methodInfo.Invoke(obj: null, parameters: parameters);
+                //MGN.ReflectionTests.Assembly.Test("MGN.ReflectionTests");// //methodInfo.Invoke(obj: null, parameters: parameters);
+            }
+            catch (TargetInvocationException targetInvocationException)
+            {
+                caughtException = targetInvocationException;
+            }
+            if (shouldFail)
+            {
+                Assert.IsNotNull(value: caughtException, message: methodInfo.Name + "(" + String.Join<Object>(", ", parameters) + ") should throw an Exception.");
+                var innerException = caughtException.InnerException ?? caughtException;
+                Assert.IsInstanceOfType(
+                    value: innerException,
+                    expectedType: typeof(AssertFailedException),
+                    message: methodInfo.Name + "(" + String.Join<Object>(", ", parameters) + ") should throw an AssertFailedException.");
+            }
+            if (!shouldFail)
+            {
+                Assert.IsNotNull(value: resultObject, message: methodInfo.Name + "(" + String.Join<Object>(", ", parameters) + ") should not be null.");
+            }
+            return resultObject;
+        }
+
+
+    }
+}
